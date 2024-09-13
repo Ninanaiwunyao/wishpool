d6cc58538efb3da722f3bdacdf80c87ec007e719
Nina Yao <nini88518@gmail.com>
2024-09-13 17:32:48 +0800

    feat: completed homepage layout

--- src/api.js
+++ src/api.js
@@ -1,3 +1,18 @@
-const getData = () => {};
+import { db } from "./firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
-export { getData };
+const getUserData = async () => {
+  try {
+    const querySnapShot = await getDocs(collection(db, "user"));
+    const data = querySnapShot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return data;
+  } catch (error) {
+    console.error("Error fetching data from firestore:", error);
+    return [];
+  }
+};
+
+export { getUserData };
