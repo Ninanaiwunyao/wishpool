97434598a0b89c22a35daffa5d7abfabf011d16e
Nina Yao <nini88518@gmail.com>
2024-09-24 14:42:49 +0800

    fix: category page layout

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -25,11 +25,11 @@ const Category = () => {
   }
 
   return (
-    <div className="bg-darkBlue relative w-full h-screen flex flex-col items-center justify-start pt-32">
+    <div className="bg-darkBlue relative w-full h-fit flex flex-col items-center justify-start pt-32">
       <h2 className="text-2xl font-bold text-cream mb-4">
         {category}的所有願望
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 h-screen mb-24">
         {filteredWishes.length > 0 ? (
           filteredWishes.map((wish) => <WishCard key={wish.id} wish={wish} />)
         ) : (
