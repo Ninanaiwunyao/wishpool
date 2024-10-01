68ae98bc375345396f1c366cc43b6ac0c8ed06d8
Nina Yao <nini88518@gmail.com>
2024-10-01 17:56:07 +0800

    fix: change background image

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -12,41 +12,58 @@ const LandingPage = () => {
   // 創建多個隨機浮動的元素
   const [floatingElements, setFloatingElements] = useState([]);
 
-  useEffect(() => {
+  const generateFloatingElements = () => {
     const elements = [];
     for (let i = 0; i < 20; i++) {
       elements.push({
         id: i,
-        size: getRandomValue(30, 70), // 隨機大小
+        size: getRandomValue(
+          window.innerWidth * 0.02,
+          window.innerWidth * 0.04
+        ), // 隨機大小
         startX: getRandomValue(0, 100), // 隨機 X 軸初始位置（相對於螢幕寬度百分比）
         delay: getRandomValue(3, 10), // 調整漂浮動畫的隨機延遲，最小延遲為 3 秒
         duration: getRandomValue(15, 25), // 調整漂浮速度（掉落時間），範圍在 15 到 25 秒之間
       });
     }
     setFloatingElements(elements);
+  };
+  useEffect(() => {
+    generateFloatingElements(); // 初始化浮動元素
+
+    // 監聽螢幕大小變化
+    const handleResize = () => {
+      generateFloatingElements();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // 清理事件監聽器
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
-    <div className="h-full w-full bg-darkBlue text-white overflow-hidden flex justify-center">
-      <div className="relative w-[1520px]">
-        <img
-          src={backgroundImage}
-          alt="Background"
-          className="w-[1520px] h-full  mx-auto my-auto"
-          style={{ zIndex: -1 }}
-        />
-        <img
-          src={gifImage}
-          alt="wishpool"
-          className="w-[700px] h-auto absolute top-64 left-24"
-        />
-        <Link
-          to="/login"
-          className="flex justify-center items-center bg-yellow text-lightBlue font-semibold text-2xl  rounded-full px-6 py-2 hover:bg-darkYellow absolute top-[550px] right-96 w-48 h-16"
-        >
-          立即入池
-        </Link>
-      </div>
+    <div className="relative w-full h-fit">
+      <img
+        src={backgroundImage}
+        alt="Background"
+        className="w-full h-full  mx-auto my-auto"
+        style={{ zIndex: -1 }}
+      />
+      <img
+        src={gifImage}
+        alt="wishpool"
+        className=" max-w-[750px] w-[40vw] h-auto absolute top-[80px] sm:top-36 md:top-80 left-6 md:left-16 lg:top-[500px] lg:left-[200px]"
+      />
+      <Link
+        to="/login"
+        className="flex justify-center items-center bg-yellow text-lightBlue font-semibold text-[10px]  rounded-full hover:bg-darkYellow absolute top-[160px] sm:top-80 right-24 sm:right-52 md:top-[680px] md:right-[480px] lg:top-[900px] lg:right-[600px] w-16 h-6 sm:h-12 sm:w-24 md:h-24 md:w-48 sm:text-sm md:text-2xl"
+      >
+        立即入池
+      </Link>
+
       {/* Particles Background - 浮動元素 */}
       {floatingElements.map((element) => (
         <motion.img
